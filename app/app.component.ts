38288c1e76eefaad22a81458fd613243ea45712d
Thomas Rivas <rivassi.thomas@gmail.com>
2016-10-21 11:11:40 -0700

    add main files for app directory

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Meal } from './meal.model';
+
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+
+  </div>
+    `
+})
+
+export class AppComponent {
+
+}
